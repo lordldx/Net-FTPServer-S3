5158656e25b11a259ef4a808649a3bd96e784cf1
Lorenzo Dieryckx <lorenzo.dieryckx+github@gmail.com>
2013-03-04 16:23:48 +0100

    head pod in IO.pm

--- lib/Net/FTPServer/S3/IO.pm
+++ lib/Net/FTPServer/S3/IO.pm
@@ -1,3 +1,19 @@
+# -*- cperl -*-
+
+=pod
+
+=head1 NAME
+
+Net::FTPServer::S3::IO - The S3 FTP server personality
+
+=head1 SYNOPSIS
+
+  use Net::FTPServer::S3::IO;
+
+=head1 METHODS
+
+=cut
+
 package Net::FTPServer::S3::IO;
 
 use Moose;
